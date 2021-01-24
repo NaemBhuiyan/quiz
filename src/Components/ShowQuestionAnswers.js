e9a47253d55ad422fc1d95397422ea771a3faa7b
Naem Bhuiyan <hopingboynaem@gmail.com>
2021-01-24 09:23:03 +0600

    final Commit

--- src/Components/ShowQuestionAnswers.js
+++ src/Components/ShowQuestionAnswers.js
@@ -38,95 +38,99 @@ const Answers = () => {
       <Row className="justify-content-center">
         <Col>
           <h1 className="text-center my-5">Give Answer</h1>
-          {questions?.map((question, index) => {
-            return (
-              <Card
-                key={question.id}
-                className={classNames({
-                  "mt-4": index > 0,
-                })}>
-                <CardBody className="bg-light">
-                  <p
-                    style={{ fontSize: "1.5rem", fontWeight: 700 }}
-                    className="mb-1">
-                    {question.text}
-                  </p>
-                  <span className="text-secondary">Asked by </span>{" "}
-                  <span className="text-info">Admin</span>
-                </CardBody>
-                <CardFooter className="d-flex justify-content-end bg-transparent border-top-0">
-                  {userType === "admin" ? (
-                    <>
+          {questions.length ? (
+            questions?.map((question, index) => {
+              return (
+                <Card
+                  key={question.id}
+                  className={classNames({
+                    "mt-4": index > 0,
+                  })}>
+                  <CardBody className="bg-light">
+                    <p
+                      style={{ fontSize: "1.5rem", fontWeight: 700 }}
+                      className="mb-1">
+                      {question.text}
+                    </p>
+                    <span className="text-secondary">Asked by </span>{" "}
+                    <span className="text-info">Admin</span>
+                  </CardBody>
+                  <CardFooter className="d-flex justify-content-end bg-transparent border-top-0">
+                    {userType === "admin" ? (
+                      <>
+                        <Button
+                          className="ml-2"
+                          color="danger"
+                          onClick={() => {
+                            questionsDispatch({
+                              type: "REMOVE",
+                              id: question.id,
+                              payload: question,
+                            });
+                          }}>
+                          Delete
+                        </Button>
+                        <Button
+                          className="ml-2"
+                          color="primary"
+                          onClick={() => {
+                            setEditQuestionModal(true);
+                            setQuestion(question);
+                            setQuestionId(question.id);
+                          }}>
+                          Edit
+                        </Button>
+                      </>
+                    ) : (
                       <Button
-                        className="ml-2"
-                        color="danger"
-                        onClick={() => {
-                          questionsDispatch({
-                            type: "REMOVE",
-                            id: question.id,
-                            payload: question,
-                          });
-                        }}>
-                        Delete
-                      </Button>
-                      <Button
-                        className="ml-2"
                         color="primary"
                         onClick={() => {
-                          setEditQuestionModal(true);
-                          setQuestion(question);
+                          setAnswerModal(true);
                           setQuestionId(question.id);
                         }}>
-                        Edit
+                        Answer
                       </Button>
-                    </>
-                  ) : (
-                    <Button
-                      color="primary"
-                      onClick={() => {
-                        setAnswerModal(true);
-                        setQuestionId(question.id);
-                      }}>
-                      Answer
-                    </Button>
-                  )}
-                </CardFooter>
-                <hr className="my-0" />
+                    )}
+                  </CardFooter>
+                  <hr className="my-0" />
 
-                <p className="text-warning pl-3">Top Answer</p>
-                {question.answers.length ? (
-                  <ListGroup>
-                    {question.answers.map((answer) => {
-                      return (
-                        <ListGroupItem key={answer.id}>
-                          <ListGroupItemHeading style={{ fontSize: "1rem" }}>
-                            Login user
-                          </ListGroupItemHeading>
-                          <ListGroupItemText className="mt-3">
-                            {answer.text}
-                          </ListGroupItemText>
-                          {userType === "user" && (
-                            <Button
-                              className="ml-2"
-                              color="danger"
-                              onClick={() => {
-                                setEditAnswerModal(true);
-                                setGivenAnswer(answer);
-                                setGivenAnswerId(answer.id);
-                              }}>
-                              Edit
-                            </Button>
-                          )}
-                        </ListGroupItem>
-                      );
-                    })}
-                  </ListGroup>
-                ) : (
-                  <p className="text-danger pl-3">No answer given yet</p>
-                )}
-              </Card>
-            );
-          })}
+                  <p className="text-warning pl-3">Top Answer</p>
+                  {question.answers.length ? (
+                    <ListGroup>
+                      {question.answers.map((answer) => {
+                        return (
+                          <ListGroupItem key={answer.id}>
+                            <ListGroupItemHeading style={{ fontSize: "1rem" }}>
+                              Login user
+                            </ListGroupItemHeading>
+                            <ListGroupItemText className="mt-3">
+                              {answer.text}
+                            </ListGroupItemText>
+                            {userType === "user" && (
+                              <Button
+                                className="ml-2"
+                                color="danger"
+                                onClick={() => {
+                                  setEditAnswerModal(true);
+                                  setGivenAnswer(answer);
+                                  setGivenAnswerId(answer.id);
+                                }}>
+                                Edit
+                              </Button>
+                            )}
+                          </ListGroupItem>
+                        );
+                      })}
+                    </ListGroup>
+                  ) : (
+                    <p className="text-danger pl-3">No answer given yet</p>
+                  )}
+                </Card>
+              );
+            })
+          ) : (
+            <h3>No Questions Asked</h3>
+          )}
         </Col>
       </Row>
       <AnswerModal
